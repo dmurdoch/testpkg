3640037889c047adbdb4dc065770a2e7611badfb
Duncan Murdoch <murdoch.duncan@gmail.com>
2023-05-08 19:47:57 -0400

    Show that levels.no() is treated as a method when levels() is called in the package.

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,8 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+levels.no <- function(xx, ...) {
+  stop("not a method")
+}
 
-hello <- function() {
-  print("Hello, world!")
+f <- function() {
+  x <- structure(1, class = "no")
+  levels(x)
 }
